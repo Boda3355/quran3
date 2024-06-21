bd6859d8475931a25361b12631bbd1074cf1cbe5
Boda3355 <elmassry826@gmail.com>
2024-06-21 23:37:51 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -16,28 +16,46 @@ module.exports = {
       console.log((`Servers: ${client.guilds.cache.size}`).magenta, (`Users: ${client.guilds.cache
         .reduce((a, b) => a + b.memberCount, 0)
         .toLocaleString()}`).yellow, (`Commands: ${client.commands.size}`).green);
-      client.user.setStatus("idle")
-      client.user.setActivity(`The Holy Quran`, { type: ActivityType.Listening })
+
+      // Initial set status and activity
+      client.user.setStatus("idle");
+      
+      let activities = [
+        ` in ${client.guilds.cache.size} servers.`,
+        `The Holy Quran `
+      ];
+      
+      let activityIndex = 0;
+      client.user.setActivity(activities[activityIndex], { type: ActivityType.Listening });
+
+      // Update activity in a loop
+      setInterval(() => {
+        activityIndex = (activityIndex + 1) % activities.length;
+        client.user.setActivity(activities[activityIndex], { type: ActivityType.Listening });
+      }, 40000  ); // 60000 milliseconds = 1 minute
+
+      // Check and join voice channels every 7 seconds
       setInterval(async () => {
         client.guilds.cache.forEach(async g => {
-          let vch = await db.get(`24_7_${g.id}`)
+          let vch = await db.get(`24_7_${g.id}`);
           if (vch == null) return;
           let ch = client.channels.cache.get(vch);
-          if (ch == null) return db.delete(`24_7_${g.id}`)
+          if (ch == null) return db.delete(`24_7_${g.id}`);
           const clientMember = g.members.cache.get(client.user.id);
           const checkJoined = clientMember?.voice?.channelId == ch.id;
           if (!checkJoined) {
-            console.log(checkJoined)
+            console.log(checkJoined);
             try {
-              await distube.voices.join(ch)
+              await distube.voices.join(ch);
             } catch (e) {
               console.log("connection", e);
             }
           }
-        })
-      }, 7000)
+        });
+      }, 7000);
+
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
   }
 };
